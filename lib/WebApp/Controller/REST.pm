dd41c499d3a51916ca3f4acdec8eb01d8c007ee5
Jussi Kinnula <jussi.kinnula@frantic.com>
2015-03-11 11:51:50 +0200

    Add paging.

--- lib/WebApp/Controller/REST.pm
+++ lib/WebApp/Controller/REST.pm
@@ -16,15 +16,30 @@ sub request : Local : ActionClass("REST") {
 
 sub request_GET {
     my ($self, $c) = @_;
-    my @requests;
-    for my $request ($c->model('DB::Request')->all) {
-        push @requests, {
+    my $params ||= $c->req->data || $c->req->params;
+    my $requests = $c->model('DB::Request');
+
+    $requests = $requests->search(undef,{
+        order_by => [
+            { '-DESC' => 'requested' }
+        ],
+        page => $params->{'page'} || 1,
+        rows => $params->{'rows'} || 50,
+    });
+    my @requests_data;
+    for my $request ($requests->all) {
+        push @requests_data, {
             requested => $request->created->strftime('%Y-%m-%dT%H:%M:%S%z'),
             parameters => from_json($request->parameters),
         };
     }
     $self->status_ok( $c, entity => {
-        requests => \@requests,
+        requests => \@requests_data,
+        pager => {
+            current_page => $requests->pager->current_page+0,
+            total_entries => $requests->pager->total_entries+0,
+            entries_per_page => $requests->pager->entries_per_page+0,
+        }
     });
 }
 
